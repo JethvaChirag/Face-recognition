ebbb431258d2d547ffa4d43fc1a4a99a83472e96
JethvaChirag <43982278+JethvaChirag@users.noreply.github.com>
2020-01-31 17:19:04 +0530

    with Validation

--- facereco/static/js/base.js
+++ facereco/static/js/base.js
@@ -1,27 +1,30 @@
 $(document).on('submit','#profile',function(e){
-
+    var nullValues=0;
     var input=document.querySelectorAll('input')
     for(var i =0;i<input.length;i++){
         if(input[i].value==''){
-            input[i].style.background='red';
+            alert(input[i].name+' cannot be null.');
+            nullValues+=1;
         }
     }
 
     e.preventDefault();
-
-    $.ajax({
-        type:'POST',
-        url:'/user/addPerson',
-        data:{
-            fname:$('#fname').val(),
-            lname:$('#lname').val(),
-            mno:$('#mno').val(),
-            intime:$('#intime').val(),
-            outtime:$('#outtime').val(),
-            csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
-        },
-        success:function(){
-            alert("Created new user");
-        }
-    });
+    if(nullValues==0){
+        $.ajax({
+            type:'POST',
+            url:'/user/addPerson',
+            data:{
+                fname:$('#fname').val(),
+                lname:$('#lname').val(),
+                mno:$('#mno').val(),
+                intime:$('#intime').val(),
+                outtime:$('#outtime').val(),
+                csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val()
+            },
+            success:function(){
+                alert("Created new user");
+            }
+        });
+    }
+    
 })
