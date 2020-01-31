6f90573cdabf83c3a6a5bbd37a3faeec7581c2fc
JethvaChirag <43982278+JethvaChirag@users.noreply.github.com>
2020-01-31 17:26:10 +0530

    with validation

--- facereco/static/js/base.js
+++ facereco/static/js/base.js
@@ -1,15 +1,24 @@
 $(document).on('submit','#profile',function(e){
     var nullValues=0;
+    var mobilenumber=false;
     var input=document.querySelectorAll('input')
     for(var i =0;i<input.length;i++){
         if(input[i].value==''){
             alert(input[i].name+' cannot be null.');
             nullValues+=1;
         }
+        if(input[i].id=='mno'){
+            if(input[i].value.length==10){
+                mobilenumber=true;
+            }
+            else{
+                alert('Please enter proper mobile number.');
+            }
+        }
     }
 
     e.preventDefault();
-    if(nullValues==0){
+    if(nullValues==0 & mobilenumber){
         $.ajax({
             type:'POST',
             url:'/user/addPerson',
